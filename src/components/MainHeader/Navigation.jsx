0e9013d5c7b3245bfc99e28102e937b7551b4368
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2022-11-22 18:24:26 +0100

    Pass global state with useContext function

--- src/components/MainHeader/Navigation.jsx
+++ src/components/MainHeader/Navigation.jsx
@@ -1,29 +1,25 @@
+import { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
 import classes from './Navigation.module.css';
 
 const Navigation = ({ onLogout }) => {
+  const ctx = useContext(AuthContext);
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && <button onClick={onLogout}>Logout</button>}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && <button onClick={onLogout}>Logout</button>}
+      </ul>
+    </nav>
   );
 };
 
