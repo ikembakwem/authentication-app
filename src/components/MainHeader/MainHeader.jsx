5aba7eb86f42d815163f1b6ed00fd018d59bfa2c
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2022-11-22 10:47:34 +0100

    Construct Navigation and MainHeader component

--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -1,5 +1,13 @@
 import classes from './MainHeader.module.css';
+import Navigation from './Navigation';
 
-const MainHeader = () => {};
+const MainHeader = ({ isAuthenticated }) => {
+  return (
+    <header className={classes['main-header']}>
+      <h1>A typical page</h1>
+      <Navigation isLoggedIn={isAuthenticated} onLogout={onLogout} />
+    </header>
+  );
+};
 
 export default MainHeader;
--- src/components/MainHeader/Navigation.jsx
+++ src/components/MainHeader/Navigation.jsx
@@ -1,5 +1,23 @@
 import classes from './Navigation.module.css';
 
-const Navigation = () => {};
+const Navigation = ({ isLoggedIn, onLogout }) => {
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        {isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {isLoggedIn && <button onClick={onLogout}>Logout</button>}
+      </ul>
+    </nav>
+  );
+};
 
 export default Navigation;
